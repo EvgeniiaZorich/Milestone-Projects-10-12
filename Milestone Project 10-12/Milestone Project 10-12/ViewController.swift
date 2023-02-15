93240cac38091f65f3fdb2a35d9c44701ea4a61d
Evgeniia Zorich <vasileva.evg.al@gmail.com>
2023-02-15 21:02:04 +0100

    initial commit

---
+++ Milestone Project 10-12/Milestone Project 10-12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Milestone Project 10-12
+//
+//  Created by Евгения Зорич on 15.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
