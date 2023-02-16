c6df6e8d7c43c05dcd262b9899b0fa5b49c6049f
Evgeniia Zorich <vasileva.evg.al@gmail.com>
2023-02-16 17:54:50 +0100

    feature Day 50

---
+++ Milestone Project 10-12/Milestone Project 10-12/Picture.swift
@@ -0,0 +1,13 @@
+//
+//  Picture.swift
+//  Milestone Project 10-12
+//
+//  Created by Евгения Зорич on 15.02.2023.
+//
+
+import Foundation
+
+struct Picture: Codable {
+    var imageID: String
+    var imageName: String
+}
